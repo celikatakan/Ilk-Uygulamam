1a5e47822d26970a7e8ca7818c88bbdb81523b66
auro <auro@DESKTOP-AV8I9BT>
2024-07-18 10:27:50 +0300

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,14 @@
+﻿using System; 
+
+namespace HelloWorld
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Atakan Celik");
+            Console.WriteLine("Yazilim kariyerime saglam adimlarla baslamak icin Patika'nin uzman kadrosu ile giris yapabilecegim icin bu bootcampteyim!");
+        }
+
+    }
+}
